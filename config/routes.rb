2bbe7bef2e85618d4e22b6195ce7c7a2d353c96d
HanashiroNarumi <168202959+HanaNaru@users.noreply.github.com>
2024-06-20 16:19:36 +0000

    rootwokaenaitoikemasenn

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ resources :user, only: [:new, :index, :show, :edit]
 resources :book, only: [:new, :index, :show, :edit]
 devise_for :users
 root to: "homes#top"
+get 'homes/about', to: 'homes#about', as: :about
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
